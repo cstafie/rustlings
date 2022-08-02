df5a2b8345f3a11c5b92efc101dd9a21f98be49a
cristian <stafie.cristian@gmail.com>
2022-08-01 22:41:27 -0700

    did some excercises"

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -29,18 +29,34 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
 
+    let mut update_team = |team_name: &str, scored: u8, conceded: u8| {
+        match scores.get_mut(team_name) {
+            Some(team) => {
+                team.goals_scored += scored;
+                team.goals_conceded += conceded;
+            }
+            None => {
+                let team = Team {
+                    name: (*team_name).to_string(),
+                    goals_scored: scored,
+                    goals_conceded: conceded,
+                };
+                scores.insert((*team_name).to_string(), team);
+            }
+        };
+    };
+
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
         let team_1_name = v[0].to_string();
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        update_team(&team_1_name, team_1_score, team_2_score);
+        update_team(&team_2_name, team_2_score, team_1_score);
     }
+
     scores
 }
 
